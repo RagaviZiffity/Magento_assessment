6efc259d53b844d13b7cdcfde6af1edeac16b6f8
Ragavi <ragavi.ramasamy@ziffity.com>
2023-10-06 11:01:57 +0530

    v3

--- ImportCustomers/CustomCmd/Command/ImportData.php
+++ ImportCustomers/CustomCmd/Command/ImportData.php
@@ -1,4 +1,5 @@
 <?php
+
 namespace ImportCustomers\CustomCmd\Command;
 
 use Symfony\Component\Console\Command\Command;
@@ -6,37 +7,45 @@ use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Input\InputArgument;
+use Magento\Customer\Model\CustomerFactory;
+use Magento\Customer\Model\ResourceModel\Customer as ResourceModel;
 use Magento\Framework\File\Csv;
 
 class ImportData extends Command
 {
     protected $csvProcessor;
+    protected $customerRepository;
+    protected $customerFactory;
 
     public function __construct(
+        ResourceModel $customerRepository,
+        CustomerFactory $customerFactory,
         Csv $csvProcessor
     ) {
         $this->csvProcessor = $csvProcessor;
-        parent::__construct(); 
+        $this->customerRepository = $customerRepository;
+        $this->customerFactory = $customerFactory;
+        parent::__construct();
     }
-    
+
     const profile = 'profile';
 
     protected function configure()
     {
         //In commandline to get profile options
         $options = [
-        new InputOption(
-            self::profile,
-            '-p',
-            InputOption::VALUE_REQUIRED,
-            'provide csv or json format'
-        )
+            new InputOption(
+                self::profile,
+                '-p',
+                InputOption::VALUE_REQUIRED,
+                'provide csv or json format'
+            )
         ];
         $this->setName('customer:importer')
             ->setDescription('Import data of customer')
             ->setDefinition($options)
             ->addArgument('source', InputArgument::REQUIRED, 'Source file'); //source argument
-        
+
         parent::configure();
     }
 
@@ -44,52 +53,45 @@ class ImportData extends Command
     {
         $profile = $input->getOption(self::profile);
         $source = $input->getArgument('source');
-        $objectManager = \Magento\Framework\App\ObjectManager::getInstance();
-        $customerRepository = $objectManager->create('Magento\Customer\Api\CustomerRepositoryInterface');
 
         if ($profile === 'csv') {
             $csvData = $this->csvProcessor->getData($source);
-            if($csvData == null) {
+            if ($csvData == null) {
                 $output->writeln("<error>Error reading CSV file.</error>");
                 return 1;
             }
             $output->writeln("<info>Data loaded successfully.</info>");
-            foreach ($csvData as $data) 
-            {
-                $customer = $objectManager->create('Magento\Customer\Api\Data\CustomerInterface');
+            foreach ($csvData as $data) {
+                $customer = $this->customerFactory->create();
                 $customer->setFirstname($data['0']);
                 $customer->setLastname($data['1']);
                 $customer->setEmail($data['2']);
-                $customerRepository->save($customer);
+                $this->customerRepository->save($customer);
             }
             $output->writeln("<info>Customer imported successfully.</info>");
-        } 
-        elseif ($profile === 'json') {
+        } elseif ($profile === 'json') {
             $jsonData = $this->readJSON($source, $output);
-            foreach ($jsonData as $data) 
-            {
-                $customer = $objectManager->create('Magento\Customer\Api\Data\CustomerInterface');
+            foreach ($jsonData as $data) {
+                $customer = $this->customerFactory->create();
                 $customer->setFirstname($data['fname']);
                 $customer->setLastname($data['lname']);
                 $customer->setEmail($data['emailaddress']);
-                $customerRepository->save($customer);
+                $this->customerRepository->save($customer);
             }
             $output->writeln("<info>Customer imported successfully.</info>");
-        } 
-
-        else {
+        } else {
             $output->writeln('<error>Invalid profile format. Please use "csv" or "json".</error>');
-            return 1; // Return a non-zero value to indicate an error.
+            return 1; //Return a non-zero value to indicate an error.
         }
 
-        return 0; // Return 0 to indicate a successful execution.
+        return 0; //Return 0 to indicate a successful execution.
     }
 
     private function readJSON($jsonFile, OutputInterface $output)
     {
         $jsonContent = file_get_contents($jsonFile);
-        $data= json_decode($jsonContent, true);
-        if($data == null) {
+        $data = json_decode($jsonContent, true);
+        if ($data == null) {
             $output->writeln("<error>Error reading json file.</error>");
             return 1;
         }
